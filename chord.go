d1a8566a55f6c4b01261f90960335dac799a87b6
vinay <vinayy765@gmail.com>
2017-04-25 16:59:21 +0530

    Added Chord function definitions

--- chord.go
+++ chord.go
@@ -47,9 +47,9 @@ func DefaultConfig() Config {
 }
 
 func (ln *LocalNode) Init(config Config) {
-	// Generate an ID
-	ln.genId()
+	// Generate an Id
 	ln.config = config
+	ln.Id = GenHash(ln.config, ln.Address)
 	// Initialize all state
 	ln.successors = make([]*Node, ln.config.NumSuccessors)
 	ln.finger = make([]*Node, ln.config.hashBits)
@@ -58,12 +58,62 @@ func (ln *LocalNode) Init(config Config) {
 	// ln.ring.transport.Register(&ln.Node, ln)
 }
 
-func (ln *LocalNode) genId() {
+func GenHash(conf Config, address string) []byte {
 	// Use the hash funciton
-	conf := ln.config
 	hash := conf.HashFunc()
-	hash.Write([]byte(ln.Address))
+	hash.Write([]byte(address))
 
-	// Use the hash as the ID
-	ln.Id = hash.Sum(nil)
+	// Use the hash as the Id
+	return hash.Sum(nil)
+}
+
+func (ln *LocalNode) Create() {
+	ln.config.hashBits = ln.config.HashFunc().Size() * 8 //??
+
+	ln.successors[0] = &ln.Node
+	ln.predecessor = nil
+}
+
+func (ln *LocalNode) Join(address string) {
+	// var n Node
+	// n.address
+}
+
+//RPC
+func (ln *LocalNode) FindSuccessor(key string) (string, error){
+	id_hash := GenHash(ln.config, key)
+	my_hash := ln.Id
+	succ_hash := successors[0].Id
+	if (bytes.compare(id_hash,my_hash)>0 && bytes.compare(id_hash,succ_hash)<=0) {
+		return (successors[0].address, nil)
+	}
+	s_address, e := FindSuccessor_Stub(successors[0].address, key)
+	return (s_address, e)
+}
+
+//RPC
+func (ln *LocalNode) GetPredecessor() (string, error) {
+	return predecessor.Address
+}
+
+//RPC
+func (ln *LocalNode) Notify(address string) (error) {
+	return nil
+}
+
+//RPC
+func (ln *LocalNode) Ping(address string) (error) {
+	return nil
+}
+
+func (ln *LocalNode) check_predecessor() {
+	err := Ping_Stub(predecessor.address)
+	if (err!=nil) {
+		predecessor = nil
+	}
+
+}
+
+func (ln *LocalNode) stabilize() {
+	
 }
