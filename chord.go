1daad026aa90cecae04e2b0491206208b307743d
vishwesh96 <vishwesh1696@gmail.com>
2017-04-26 03:26:25 +0530

    added key delete operation

--- chord.go
+++ chord.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/rpc"
 	"time"
+	"strconv"
 	"math/rand"
 )
 
@@ -444,12 +445,12 @@ func randStabilize(conf Config) time.Duration {
 
 func (ln * LocalNode) ReadKey(key string, val *string) error{
 	var leader string
-	e := ln.FindSuccessor(key, leader *string)
+	e := ln.FindSuccessor(key, &leader)
 	if e!=nil {
 		return e
 	}
-	ln.remote_ReadKeyLeader(leader,key,val)
-
+	e = ln.remote_ReadKeyLeader(leader,key,val)
+	return e
 }
 
 func (ln *LocalNode) ReadKeyLeader(key string,val *string){
@@ -457,7 +458,7 @@ func (ln *LocalNode) ReadKeyLeader(key string,val *string){
 	
 func (ln *LocalNode) WriteKey(key string, val string) error{
 	var leader string
-	e := ln.FindSuccessor(key, leader *string)
+	e := ln.FindSuccessor(key, &leader)
 	if e!=nil {
 		return e
 	}
@@ -466,7 +467,7 @@ func (ln *LocalNode) WriteKey(key string, val string) error{
 	return e
 }
 
-func (ln * LocalNode) WriteKeyLeader(key string, val string){
+func (ln * LocalNode) WriteKeyLeader(key string, val string) error{
 		ln.data[0][key] = val
 		//check successor exists
 		e0 := ln.remote_WriteKeySuccessor(ln.successors[0].Address,key,val,1)
@@ -477,12 +478,53 @@ func (ln * LocalNode) WriteKeyLeader(key string, val string){
 		if e1!= nil {
 			return e1
 		}
+		return nil
 }
 
 func (ln * LocalNode) WriteKeySuccessor(key string, val string, replica_number int) error{
-	if len(ln.data < replica_number) {
+	if len(ln.data) < replica_number {
 		return errors.New("Not enough replicas")
 	}
 	ln.data[replica_number][key] = val
+	return nil
+}
+
+func (ln *LocalNode) DeleteKey(key string) error{
+	var leader string
+	e := ln.FindSuccessor(key, &leader)
+	if e!=nil {
+		return e
+	}
+	e = ln.remote_DeleteKeyLeader(leader,key)
+	return e
+}
+
+func (ln * LocalNode) DeleteKeyLeader(key string) error{
+		_ , ok := ln.data[0][key]
+		if ok == false{
+			return errors.New("Key not present in Leader")
+		}
+		delete(ln.data[0],key)
+		//check successor exists
+		e0 := ln.remote_DeleteKeySuccessor(ln.successors[0].Address,key,1)
+		if e0!= nil {
+			return e0
+		}
+		e1 := ln.remote_DeleteKeySuccessor(ln.successors[1].Address,key,2)
+		if e1!= nil {
+			return e1
+		}
+		return nil
+}
+
+func (ln * LocalNode) DeleteKeySuccessor(key string, replica_number int) error{
+	if len(ln.data) < replica_number {
+		return errors.New("Not enough replicas")
+	}
+	 _ , ok := ln.data[replica_number][key]
+	if ok == false{
+		return errors.New("Key not present in replica" + strconv.Itoa(replica_number))
+	}
+	delete(ln.data[replica_number],key)
 	return nil
 }
