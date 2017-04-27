543af7e963d5e4e13b7ef32d9adb404851e80f51
Agrim9 <agrim9gupta@gmail.com>
2017-04-27 15:02:32 +0530

    Beginning Transactions

--- client.go
+++ client.go
@@ -5,13 +5,67 @@ import (
     "strconv"
     "log"
     "os"
+    "container/list"
+    "fmt"
 )
 
+type Hbeat struct{ 
+	var Keys []string
+	var ret_vals []string
+	var args_vals []string
+	var types []int
+}
+func Client_Transaction() error {
+
+}
 func Client_remoteRead(address string, key string, val *string) error {
 	e,_ := remote_ReadKey(address,key,4,val)
 	return e
 }
+func (ln *LocalNode) TransactionLeader(t transaction, val* string) error {
+	var leaders []string = make([]string,len(t.Keys))
+	var replies []Hbeat = make([]Hbeat,len(t.Keys))
+	var e error
+	temp_str:=new(str)
+	for i:=range t.Keys {
+		e=ln.FindSuccessor(t.Keys[i],temp_str)
+		leaders[i]=*temp_str
+	}
+	for i:=range leaders {
+		ln.Remote_Heartbeat(leaders[i],&replies[i]) // Prepare to commit
+	}
+	Hbeat_start := time.Now() //Start Timer
+	for ((time.Since(Hbeat_start))*time.Second<ln.config.HeartBeatTime) {
+	}
+	for i:=range replies {
+		if(replies[i]==nil) {
+			//If even one value is nil, refrain to make changes, and abort
+			return errors.New("All Nodes Busy")
+		}
+	}
 
+	//Since everyone is ready go ahead with transaction
+	for i:=range t.Keys {
+		if(t.types[i]==0) {
+				err,Async_Call:=remote_ReadKey(leaders[i],t.Keys[i],0,&t.ret_vals[i])
+				if(err!=nil) {
+					log.fatal("Unexepected Error in transaction")
+				}
+				<-Async_Call.Done
+		} else {
+			if(t.types[i]==1) {
+				err := remote_WriteKey(leaders[i],t.Keys[i],t.args_vals[i],0)
+				if(err!=nil) {
+					log.fatal("Unexepected Error in transaction")
+				}
+			} else if (t.types[i]==2) {
+				err:=remote_DeleteKey(leaders[i],t.Keys[i],0)
+			}
+		}		
+	}
+	*val=t.ret_vals[t.Keys-1]
+
+}
 func (ln * LocalNode) ReadKey(key string, val *string) error{
 	leader := new(string)
 	e := ln.FindSuccessor(key, leader)
